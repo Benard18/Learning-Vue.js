e622930015ed33ade40c6a8174b7e2a14ca16dbd
Ben Karanja <benkaranja43@gmail.com>
2018-11-01 17:36:02 +0300

    This is my third test of this franchise my people

---
+++ third_test/index.vue
@@ -0,0 +1,25 @@
+var vm = new Vue({
+	el:'#app',
+	data:{
+	votes:0,
+	state:null
+	},
+	methods: {
+		increase: function () {
+			if (this.state == "Carlifornia") {
+				this.votes += 3;
+			}
+			else {
+				this.votes ++;
+			}
+		},
+		decrease: function () {
+			if (this.state == "Carlifornia") {
+				this.votes -= 3;
+			}
+			else {
+				this.votes --;
+			}
+		} 
+	}
+});
