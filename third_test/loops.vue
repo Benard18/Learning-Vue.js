51cb917b235f429d00c5a1d0984b11f6eb522b96
Ben Karanja <benkaranja43@gmail.com>
2018-11-01 17:39:03 +0300

    This is my third test of this franchise my people

---
+++ third_test/loops.vue
@@ -0,0 +1,10 @@
+var vm = new Vue({
+	el: "#app",
+	data: {
+		courses: [
+		{title:'Vue JS', instructor:'red'},
+		{title:'Laravel', instructor:'red'},
+		{title:'Javascript', instructor:'red'}
+		]
+	}
+	})
